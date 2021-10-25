8a17dde99fad7b9647ac221b2c9f7220e4dcdc71
Antonio Contreras <drancope@gmail.com>
2021-10-25 10:37:00 +0200

    Create bucles.py

---
+++ bucles.py
@@ -0,0 +1,33 @@
+
+# Bucles
+
+# bucle:
+#    hace lo que hay aquí hasta que el bucle termina de alguna forma
+
+## For 
+#
+#
+# for variable in range:
+#     Hace esto hasta que termina range
+#
+# función range:     range(desde, hasta)
+#     Hace esto paso a paso hasta el final 
+
+print("Ejemplo for")
+for i in range(10): # números del 0 al 10
+    print(i)
+
+
+# while
+#    
+#    # 1. while condición:
+#    # 2.    Hace esto mientras condición resulta verdadero
+
+print("Ejemplo while")
+a = 1
+b = 10
+while a < b:
+    print(a)
+    a += 1
+
+
