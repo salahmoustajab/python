44b07902d26ec5c6fc271cdd4d6af23fcc3630a0
Antonio Contreras <drancope@gmail.com>
2021-10-26 21:58:33 +0200

    probando atom

--- bucles.py
+++ bucles.py
@@ -1,17 +1,14 @@
 
-# Bucles
 
 # bucle:
 #    hace lo que hay aquí hasta que el bucle termina de alguna forma
 
-## For 
 #
 #
 # for variable in range:
 #     Hace esto hasta que termina range
 #
 # función range:     range(desde, hasta)
-#     Hace esto paso a paso hasta el final 
 
 print("Ejemplo for")
 for i in range(10): # números del 0 al 10
@@ -19,7 +16,6 @@ for i in range(10): # números del 0 al 10
 
 
 # while
-#    
 #    # 1. while condición:
 #    # 2.    Hace esto mientras condición resulta verdadero
 
@@ -33,7 +29,6 @@ while a < b:
 #Tu turno:
 
 #tareas:
-# 1. En el botón "version control" puedes crear tu 
 #    propia rama del repositorio
 # 2. Resuelve el problema de la tabla de multiplicar.
 # 3. Puedes hacer "commit & push" para guardar los cambios en GitHub
