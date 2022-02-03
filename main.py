63c2d989f10855021290f0a3d37294de4f48798b
salah moustajab <moustajabsalah@gmail.com>
2022-02-03 12:29:00 +0100

    asdfasdf

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 print("Hola")
+
+#FUNCIONES
+def tabla_multiplicar(numA):
+    for i in ranger(1, 11):
+        print(i, " por ", numA, " = ", i*numA)
+
+def factorial(num):
+    if (num < 2):
+        return num * 1
+    else:
+        return num * factorial(num - 1)
+        
